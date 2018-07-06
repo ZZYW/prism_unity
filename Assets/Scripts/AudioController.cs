f1133792ee7781b0fa530e98f8be5af1210fd077
Yang Wang <yang.wang.dec@gmail.com>
2018-07-06 01:21:35 -0400

    audiocontroller

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -10,8 +10,24 @@ public class AudioController : MonoBehaviour
 
     //public audio
     public AudioMixer mainMixer;
-    public AudioSource glitchAS;
-    //AudioMixerGroup[] mixerGroups;
+
+
+    public AudioSource ambient01;
+    public AudioSource ambient02;
+    public AudioSource ambient03;
+    public AudioSource scifiCommunication;
+    public AudioSource colorAmbient;
+    public AudioSource glitch;
+
+    public float ambient01DB;
+    public float ambient02DB;
+    public float ambient03DB;
+    public float scifiCommunicationDB;
+    public float colorAmbientDB;
+    public float glitchDB;
+
+
+
 
     bool inColorMode;
     bool playGlitch;
@@ -31,56 +47,85 @@ public class AudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            inColorMode = !inColorMode;
-            EnterColorMode(inColorMode);
-        }
+        ambient01DB = GetDB(ambient01);
+        ambient02DB = GetDB(ambient02);
+        ambient03DB = GetDB(ambient03);
+        scifiCommunicationDB = GetDB(scifiCommunication);
+        glitchDB = GetDB(glitch);
+        colorAmbientDB = GetDB(colorAmbient);
 
-        //if(Input.GetKeyDown)
 
-        if (Input.GetKeyDown(KeyCode.M))
+        //sum /= spectrum.Length;
+        //float avg = for(int )
+        //Debug.Log(sum);
+
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    inColorMode = !inColorMode;
+        //    EnterColorMode(inColorMode);
+        //}
+
+        ////if(Input.GetKeyDown)
+
+        //if (Input.GetKeyDown(KeyCode.M))
+        //{
+        //    if (glitch.isPlaying == false)
+        //    {
+        //        glitch.Play();
+        //    }
+        //}
+        //if (Input.GetKeyUp(KeyCode.M))
+        //{
+        //    if (glitch.isPlaying)
+        //    {
+        //        glitch.Pause();
+        //    }
+        //}
+    }
+
+    private float GetDB(AudioSource source)
+    {
+        float[] spectrum = new float[64];
+        source.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        float sum = 0;
+        for (int i = 0; i < spectrum.Length; i++)
         {
-            if (glitchAS.isPlaying == false)
-            {
-                glitchAS.Play();
-            }
+            sum += spectrum[i];
         }
-        if (Input.GetKeyUp(KeyCode.M))
-        {
-            if (glitchAS.isPlaying)
-            {
-                glitchAS.Pause();
-            }
-        }
+        return sum;
     }
 
 
     public void EnterColorMode(bool state)
     {
-        PlayGlitch(0.3f);
-        if (state)
-        {
-            mainMixer.SetFloat("colorVol", 1);
-            mainMixer.SetFloat("finalVol", 0);
-        }
-        else
-        {
-            mainMixer.SetFloat("colorVol", 0);
-            mainMixer.SetFloat("finalVol", 1);
-        }
-        //throw new System.NotImplementedException();
+        //PlayGlitch(0.3f);
+        //if (state)
+        //{
+        //    if (colorTrack.isPlaying == false)
+        //    {
+        //        colorTrack.Play();
+        //    }
+        //    mainMixer.SetFloat("colorVol", 1);
+        //    mainMixer.SetFloat("finalVol", 0);
+        //}
+        //else
+        //{
+        //    mainMixer.SetFloat("colorVol", 0);
+        //    mainMixer.SetFloat("finalVol", 1);
+        //}
+        ////throw new System.NotImplementedException();
     }
 
     void PlayGlitch(float duration)
     {
-        glitchAS.Play();
-        Invoke("StopGlitch", duration);
+        //glitchAS.Play();
+        //Invoke("StopGlitch", duration);
     }
 
     void StopGlitch()
     {
-        glitchAS.Pause();
+        //  glitchAS.Pause();
     }
 
 }
